98bdef089292ca6fce0d6a3b964637f3d57093d3
rnbarcellos <rodrigo.barcellos@outlook.com.br>
2023-03-09 16:58:23 -0300

    req01: create api request and render dogs image

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
-  componentDidMount() {
-    // Implemente sua lógica aqui
+  state = {
+    imageUrl: '',
+    isLoading: true,
+  };
+
+  async componentDidMount() {
+    this.fetchAPI();
   }
 
   shouldComponentUpdate(/* nextProps, nextState */) {
@@ -15,8 +20,23 @@ class App extends React.Component {
     // Implemente sua lógica aqui
   }
 
+  fetchAPI = async () => {
+    const endpoint = 'https://dog.ceo/api/breeds/image/random';
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    this.setState({ imageUrl: data.message, isLoading: false });
+  };
+
   render() {
-    return <h1>Doguinhos</h1>;
+    const { imageUrl, isLoading } = this.state;
+    return (
+      <div>
+        <h1>Doguinhos</h1>
+        { isLoading ? <h2>Loading...</h2>
+          : <img src={ imageUrl } alt="Doguinho aleatório" />}
+        <input type="button" value="Novo doguinho!" onClick={ this.fetchAPI } />
+      </div>
+    );
   }
 }
 
